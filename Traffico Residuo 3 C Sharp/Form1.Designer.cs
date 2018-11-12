15e1e42babf01cef05baf3c077cf51efb3b8f1db
Nicholas Martini <nicho.martini@gmail.com>
2018-11-12 20:05:44 +0100

    Aggiungere i file di progetto.

---
+++ Traffico Residuo 3 C Sharp/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Traffico_Residuo_3_C_Sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.provaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.esciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.provaToolStripMenuItem,
+            this.esciToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(196, 52);
+            // 
+            // provaToolStripMenuItem
+            // 
+            this.provaToolStripMenuItem.Name = "provaToolStripMenuItem";
+            this.provaToolStripMenuItem.Size = new System.Drawing.Size(195, 24);
+            this.provaToolStripMenuItem.Text = "Avvio automatico";
+            // 
+            // esciToolStripMenuItem
+            // 
+            this.esciToolStripMenuItem.Name = "esciToolStripMenuItem";
+            this.esciToolStripMenuItem.Size = new System.Drawing.Size(195, 24);
+            this.esciToolStripMenuItem.Text = "Esci";
+            this.esciToolStripMenuItem.Click += new System.EventHandler(this.esciToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 252);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem provaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem esciToolStripMenuItem;
+    }
+}
+
---
+++ Traffico Residuo 3 C Sharp/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace Traffico_Residuo_3_C_Sharp
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer = new Timer();
+        private WebBrowser webBrowser = new WebBrowser();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+                this.Hide();
+                this.ShowInTaskbar = false;
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                webBrowser.Navigate("http://internet.tre.it/");
+                webBrowser.ScriptErrorsSuppressed = true;
+            }
+
+            timer.Interval = 10 * 1000;
+            timer.Enabled = true;
+            timer.Tick += new EventHandler(timer_Tick);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            string s = TrafficoResiduo();
+
+            try
+            {
+                if (s != "Ti rimangono 500 MB di 1 GB")
+                {
+                    Notifica(ToolTipIcon.Info, "Traffico Residuo Giornaliero", s + " (" + Fuffa(s) + " GB previsti)");
+                    timer.Enabled = false;
+                }
+                else
+                {
+                    Notifica(ToolTipIcon.Info, "Traffico Residuo Giornaliero", "Sito offline");
+                    timer.Enabled = false;
+                }
+            }
+            catch
+            {
+                Notifica(ToolTipIcon.Info, "Traffico Residuo Giornaliero", "Impossibile visualizzare il traffico residuo");
+            }
+            
+
+        }
+
+        private void esciToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private string TrafficoResiduo()
+        {
+            string s = "";
+
+            try
+            {
+                HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                htmlDocument.LoadHtml(webBrowser.Document.Body.OuterHtml);
+                HtmlNode htmlNode = htmlDocument.DocumentNode.SelectSingleNode("//div[@id='divPercentageText']");
+
+                s = htmlNode.InnerText;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return s;
+        }
+
+        private void Notifica(ToolTipIcon icona, string titolo, string messaggio)
+        {
+            notifyIcon1.BalloonTipIcon = icona;
+            notifyIcon1.BalloonTipTitle = titolo;
+            notifyIcon1.BalloonTipText = messaggio;
+            notifyIcon1.ShowBalloonTip(5000);
+        }
+
+        private double Fuffa(string traffico)
+        {
+            var trafficoRimanente = traffico;
+            string[] parti = trafficoRimanente.Split();
+            decimal trafficoMassimoGiornaliero = Convert.ToDecimal(parti[5]) / DateTime.DaysInMonth(DateTime.Today.Year, DateTime.Today.Month);
+            decimal trafficoPrevisto = Convert.ToDecimal(parti[5]) - (trafficoMassimoGiornaliero * DateTime.Today.Day);
+            return Convert.ToDouble(Math.Round(trafficoPrevisto, 2));
+        }
+    }
+}
